fb8d6d3cc51a4871eb4eba37b8fce7b310101ff9
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-05-04 20:34:19 +0300

    Update MyTextView_Roboto_Black.java

--- app/src/main/java/com/quintus/labs/datingapp/customfonts/MyTextView_Roboto_Black.java
+++ app/src/main/java/com/quintus/labs/datingapp/customfonts/MyTextView_Roboto_Black.java
@@ -5,14 +5,6 @@ import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.widget.TextView;
 
-
-/**
- * DatingApp
- * https://github.com/quintuslabs/DatingApp
- * Created on 25-sept-2018.
- * Created by : Santosh Kumar Dash:- http://santoshdash.epizy.com
- */
-
 public class MyTextView_Roboto_Black extends TextView {
 
     public MyTextView_Roboto_Black(Context context, AttributeSet attrs, int defStyle) {
