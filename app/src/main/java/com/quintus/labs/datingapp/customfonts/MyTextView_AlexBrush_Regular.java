c6f931ae9a360383046fd445bf964f8a31dbdf93
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-05-04 20:32:11 +0300

    Update MyTextView_AlexBrush_Regular.java

--- app/src/main/java/com/quintus/labs/datingapp/customfonts/MyTextView_AlexBrush_Regular.java
+++ app/src/main/java/com/quintus/labs/datingapp/customfonts/MyTextView_AlexBrush_Regular.java
@@ -6,13 +6,6 @@ import android.util.AttributeSet;
 import android.widget.TextView;
 
 
-/**
- * DatingApp
- * https://github.com/quintuslabs/DatingApp
- * Created on 25-sept-2018.
- * Created by : Santosh Kumar Dash:- http://santoshdash.epizy.com
- */
-
 public class MyTextView_AlexBrush_Regular extends TextView {
 
     public MyTextView_AlexBrush_Regular(Context context, AttributeSet attrs, int defStyle) {
