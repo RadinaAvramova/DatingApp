cd1ee3634c8dd832937ef37e3c06e10fb671d662
Radina Avramova <99686592+RadinaAvramova@users.noreply.github.com>
2024-05-04 20:12:01 +0300

    Update IntroductionMain.java

--- app/src/main/java/com/quintus/labs/datingapp/Introduction/IntroductionMain.java
+++ app/src/main/java/com/quintus/labs/datingapp/Introduction/IntroductionMain.java
@@ -10,14 +10,6 @@ import com.quintus.labs.datingapp.Login.Login;
 import com.quintus.labs.datingapp.Login.RegisterBasicInfo;
 import com.quintus.labs.datingapp.R;
 
-
-/**
- * DatingApp
- * https://github.com/quintuslabs/DatingApp
- * Created on 25-sept-2018.
- * Created by : Santosh Kumar Dash:- http://santoshdash.epizy.com
- */
-
 public class IntroductionMain extends AppCompatActivity {
 
     private Button signupButton;
